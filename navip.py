a6366079c24908f14cf3ac9690ad2949c2d53b95
janbaasner <janbaas@cebitec.uni-bielefeld.de>
2019-03-25 15:56:38 +0100

    cInDels v2

--- navip.py
+++ navip.py
@@ -120,21 +120,10 @@ if __name__ == '__main__':
 
         elif args[args.index("--mode")+1] == "sfa" \
                 and "--innavipvcf" in args\
-                and "--outpath" in args\
-                and "--bpr" in args:
+                and "--outpath" in args:
             print("Start: simple first analysis")
-            if not overwriting:
-                try:
-                    file = open(args[args.index("--outpath") + 1] + "no_NONE.vcf", "r")
-                    file.close()
-                    sys.exit("There already exist some output-data in: " + args[args.index("--outpath") + 1])
-
-                except:
-                    pass
             try:
                 file = open(args[args.index("--innavipvcf")+1],'r')
-                file.close()
-                file = open(args[args.index("--innavipfasta") + 1], 'r')
                 file.close()
                 try:
                     file = open(args[args.index("--outpath") + 1] + "try-file-for-exceptions", "r")
@@ -155,13 +144,58 @@ if __name__ == '__main__':
             every_cindel_compensation = True
             if "--ecc" in args:
                 every_cindel_compensation: False
+            picture_formats = "pdf"
+            if "--format" in args:
+                picture_formats = args[args.index("--format")+1]
+
+
+            if not overwriting:
+                mode_name = ""
+                if every_cindel_compensation:
+                    mode_name = "cInDels"
+                else:
+                    mode_name = "only_additive_cInDels"
+
+                all_the_output_file_names = []
+
+                table_outputname1 = "transcripts_isoform_" + mode_name + '.txt'
+                table_outputname2 = "transcripts_isoform_" + mode_name + '_TIDs' + '.txt'
+                table_outputname3 = "transcripts_unique_" + mode_name + '.txt'
+                table_outputname4 = "transcripts_unique_" + mode_name + '_TIDs' + '.txt'
+                table_outputname5 = "transcripts_unique_" + mode_name + '_TIDs_detailed' + '.txt'
+                table_outputname6 = "transcripts_unique_" + mode_name + '_TIDs_detailed_sorted_by_TID' + '.txt'
+
+                all_the_output_file_names.append(table_outputname1)
+                all_the_output_file_names.append(table_outputname2)
+                all_the_output_file_names.append(table_outputname3)
+                all_the_output_file_names.append(table_outputname4)
+                all_the_output_file_names.append(table_outputname5)
+                all_the_output_file_names.append(table_outputname6)
+
+                outputfolder = args[args.index("--outpath") + 1]
+
+                probably_supported_formats = ["png", "pdf", "ps", "eps", "svg"]
+                for orig_outputname in [table_outputname1,table_outputname3]:
+                    for picture_format in picture_formats.split(','):
+                        if picture_format.lower() in probably_supported_formats:
+                            try:
+                                open(outputfolder + orig_outputname.split(".")[0] + "." + picture_format.lower(), 'r')
+                                sys.exit("File is existing and overwriting is not active:" + outputfolder + orig_outputname.split(".")[0] + "." + picture_format.lower())
+                            except FileNotFoundError:
+                                continue
+                for file_name in all_the_output_file_names:
+                    try:
+                        open(outputfolder + file_name, 'r')
+                        sys.exit("File is existing and overwriting is not active:" + outputfolder + file_name)
+                    except FileNotFoundError:
+                        continue
 
             #doku for all files, new names, too (better files, too)
             # vcf_file_link:str, mod_or_not: bool, outputfolder: str, max_bp_range: int
             sfa2.sfa2_main(args[args.index("--innavipvcf") + 1],
                         every_cindel_compensation,
                         args[args.index("--outpath") + 1],
-                        int(args[args.index("--bpr") + 1]) )
+                           picture_formats)
         elif args[args.index("--mode")+1] == "vcfc" \
                 and "--invcf" in args\
                 and "--outpath" in args:
